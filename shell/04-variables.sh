0d38bdb314e96c9d715db89ea7149497225b7da0
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2025-06-14 18:13:19 +0530

    shell practice

--- shell/04-variables.sh
+++ shell/04-variables.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
+# function and script level $ will consider like this  , it depends on where u are giving
 PERSON1=$1
 PERSON2=$2
-
-PERSON3=3
-
 
 echo "$PERSON1 : Hi $PERSON2, How are you?"
 echo "$PERSON2 : Hello $PERSON1, i am good. how are you doing?"
@@ -15,5 +12,3 @@ echo "$1 : Hi $2, How are you?"
 echo "$2 : Hello $1, i am good. how are you doing?"
 echo "$1 : I am doing good too. where are you going?"
 echo "$2 : going to delhi yaar chalo bye"
-echo "$1 Hi $PERSON3"
-echo "Hi $3"
---
+++ shell/05-variables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "please enter your username::"
+
+read -s USERNAME #takes input into USERNAME variable
+
+echo "Username entered is:: $USERNAME"
+
+echo "please enter your password::"
+
+read -s PASSWD
+
+echo "your password is PASSWD"
+ECHO "GOOGLE"
+
+read -s otp
+
+echo "your otp is $otp"
