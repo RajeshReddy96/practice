c72546c715b8142f6da9d125269376f652fc4db7
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2025-06-14 18:30:46 +0530

    shell practice

--- shell/06-array.sh
+++ shell/06-array.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-FRUITS=("APPLE" "BANANA" ""KIWI)
-VEGETABLES=("CARROT","BEETROOT","BRINGAL")
+FRUITS=("APPLE" "BANANA" "KIWI")
 
 echo "First fruit is : ${FRUITS[0]}"
 echo "second fruit is : ${FRUITS[2]}"
-echo "vegetable is : ${VEGETABLES[2]}"
 echo "fruits all : ${FRUITS[@]}"
-echo "vegetables are all : $VEGETABLES[@]"
