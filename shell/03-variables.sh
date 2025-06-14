25e47bb250494048272259d136c17af0d5ca8af3
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2025-06-14 17:48:52 +0530

    shell practice

---
+++ shell/03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Rajesh
+PERSON2=Suresh
+
+echo "$PERSON1 : Hi ${PERSON2}, How are you?"
+echo "${PERSON2} : Hello $PERSON1, i am good. how are you doing?"
+echo "$PERSON1 : I am doing good too. where are you going?"
+echo "${PERSON2} : going to delhi yaar chalo bye"
+
