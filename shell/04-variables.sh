3cf0c88cdf6b920e8852e4955d54b8060bc6c61d
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2025-06-14 18:03:29 +0530

    shell practice

--- shell/04-variables.sh
+++ shell/04-variables.sh
@@ -3,6 +3,9 @@
 PERSON1=$1
 PERSON2=$2
 
+PERSON3=3
+
+
 echo "$PERSON1 : Hi $PERSON2, How are you?"
 echo "$PERSON2 : Hello $PERSON1, i am good. how are you doing?"
 echo "$PERSON1 : I am doing good too. where are you going?"
@@ -12,3 +15,5 @@ echo "$1 : Hi $2, How are you?"
 echo "$2 : Hello $1, i am good. how are you doing?"
 echo "$1 : I am doing good too. where are you going?"
 echo "$2 : going to delhi yaar chalo bye"
+echo "$1 Hi $PERSON3"
+echo "Hi $3"
